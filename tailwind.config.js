62381458d9623782485ff085a7714c8f1e97b8d0
PerdiDev <perdidev@gmail.com>
2023-01-11 01:09:18 -0800

    Menambahkan interaksi animasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,52 @@ module.exports = {
         'primary': '#E3AA18',
         'greyness': '#444341',
       },
+      transitionTimingFunction: {
+        'in-expo': 'cubic-bezier(0.8, 0, 1, 1)',
+        'out-expo': 'cubic-bezier(0, 0, 0.2, 1)',
+      },
+      keyframes: {
+        'more': {
+            '0%, 100%': {
+              transform: 'translateX(25%)',
+            },
+            '50%': {
+              transform: 'translateX(0)',
+            },
+        },
+        'avos': {
+            '0%': {
+              transform: 'translate(-25%)',
+            },
+            '25%': {
+              transform: 'translate(-25%, 25%)',
+            },
+            '50%': {
+              transform: 'translate(0%, 25%)',
+            },
+            '75%': {
+              transform: 'translate(0%, 0%)',
+            },
+            '100%': {
+              transform: 'translate(-25%)',
+            },
+        },
+        'slide': {
+          '0%': {
+            scale: '0',
+          },
+          '100%': {
+            scale: '1',
+          }
+        }
+      },
+      animation: {
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+        'spin-slow': 'spin 3s linear infinite',
+        'more': 'more 1s linear infinite',
+        'space': 'avos 3s ease-in infinite',
+        'swosh': 'slide 1s ease-in 1'
+      },
     },
   },
   plugins: [],
